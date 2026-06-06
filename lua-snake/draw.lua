69dca89570012fca65e39fe819ac220602a8101c
devrisby <131298724+rezbee-dev@users.noreply.github.com>
2026-06-06 09:48:40 -0400

    refactor drawing logic to its own module

---
+++ lua-snake/draw.lua
@@ -0,0 +1,24 @@
+local Draw = {}
+
+function Draw.playableArea(width, height, offset,color)
+    love.graphics.setColor(color)
+    love.graphics.rectangle("fill", offset,offset,width,height)
+    love.graphics.setColor(1,1,1)
+    love.graphics.setLineWidth(4)
+    -- Love2D draws lines centered on the coordinates. A 4-pixel wide border will extend 2 pixels inside and 2 pixels outside your specified dimensions.
+    love.graphics.rectangle("line", offset-2,offset-2,width+2,height+2)
+end
+
+function Draw.snake(coords, bodyColor, headColor, offset, cellSize)
+    love.graphics.setColor(headColor)
+    love.graphics.rectangle("fill", offset+(coords[1].x * cellSize), offset+(coords[1].y * cellSize), cellSize-1, cellSize-1)
+
+    love.graphics.setColor(bodyColor)
+    for i, position in ipairs(coords) do
+        if i ~= 1 then
+            love.graphics.rectangle("fill", offset+(position.x * cellSize), offset+(position.y * cellSize), cellSize-1, cellSize-1)
+        end
+    end
+end
+
+return Draw
--- lua-snake/main.lua
+++ lua-snake/main.lua
@@ -1,47 +1,36 @@
 -- based on tutorial, https://berbasoft.com/simplegametutorials/love/snake/
-Game = {
-    area = {
-        x = 100,
-        y = 100,
-        x2 = 600,
-        y2=400
-    }
+
+local draw = require("draw")
+
+GAME = {
+    AREA = {
+        OFFSET=100,
+        WIDTH=600,
+        HEIGHT=400
+    },
+    AREA_COLOR = {76/255,165/255,224/255},
+    CELLSIZE=20
 }
 
-Cellsize = 20
+SNAKE = {
+    COORDS = {
+        {x = 3, y = 1},
+        {x = 2, y = 1},
+        {x = 1, y = 1},
+    },
+    BODY_COLOR = {0,0.80,0.55},
+    HEAD_COLOR = {0,1,0}
+}
 
 function love.load()
     love.window.setMode(800,600, {vsync=1})
 end
 
 function love.draw()
-    -- love.graphics.print("hello wolrd!", 400, 300)
-    love.graphics.setColor(1,1,1)
-    love.graphics.rectangle("line", Game.area.x,Game.area.y,Game.area.x2,Game.area.y2)
-    Draw_snake()
+    draw.playableArea(GAME.AREA.WIDTH, GAME.AREA.HEIGHT, GAME.AREA.OFFSET,GAME.AREA_COLOR)
+    draw.snake(SNAKE.COORDS, SNAKE.BODY_COLOR, SNAKE.HEAD_COLOR, GAME.AREA.OFFSET,GAME.CELLSIZE)
 end
 
 function love.update(dt)
 end
 
-function Draw_snake()
-    local snake_segments = {
-        {x = 3, y = 1},
-        {x = 2, y = 1},
-        {x = 1, y = 1},
-    }
-
-    love.graphics.setColor(0,1,0)
-    -- love.graphics.rectangle("fill", Game.area.x + 5, Game.area.y+5, Game.area.x+10, Game.area.y+10)
-    -- love.graphics.rectangle("fill", 5, 5, 20, 20)
-    for i,position in ipairs(snake_segments) do
-        -- love.graphics.rectangle("fill", Game.area.x + (5*i), Game.area.y+5, Cellsize-2,Cellsize-2)
-        love.graphics.rectangle("fill", Game.area.x + (position.x * Cellsize), Game.area.y + (position.y * Cellsize), Cellsize-2,Cellsize-2)
-        -- Draw_snake_segment((Game.area.x + (5*i)), Game.area.y+5, 2)
-    end
-end
-
-function Draw_snake_segment(x,y,offset)
-    love.graphics.rectangle("fill", x-offset, y, Cellsize-offset,Cellsize)
-
-end
